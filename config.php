77557f92b991fad8515df01a3cef5bb0b2a1806e
HB-booba <35043477+HB-booba@users.noreply.github.com>
2023-07-13 20:09:06 +0100

    First Commit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+
+// Product Details
+// Minimum amount is $0.50 US
+$itemName = "Demo Product";
+$itemPrice = 100;
+$currency = "USD";
+
+/* Stripe API configuration
+ * Remember to switch to your live publishable and secret key in production!
+ * See your keys here: https://dashboard.stripe.com/account/apikeys
+ */
+const STRIPE_API_KEY = 'sk_test_51Mj1unIyLXu55jcPmaVKe6WvWFXM8yJS3nMlnBDBxFwmtHkAmAuwqJ8irILXG170d6pMZ38STvAAqqMsWKZNI0rD00v9up0bzN';
+const STRIPE_PUBLISHABLE_KEY = 'pk_test_51Mj1unIyLXu55jcPKhiUtIIqXM6QMKSJwvIRl6QYbo84lJ3o7iy2t9XD3KzYKRcUbYQFHkT83b05JNAjOaVJmyHu00gRez0mCB';
+
+// Database configuration
+const DB_HOST = 'localhost';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = 'root';
+const DB_NAME = 'strip';
+
+
+
+
+//Clé publique :    pk_test_51Mj1unIyLXu55jcPKhiUtIIqXM6QMKSJwvIRl6QYbo84lJ3o7iy2t9XD3KzYKRcUbYQFHkT83b05JNAjOaVJmyHu00gRez0mCB
+//
+//
+//Clé secrète :  sk_test_51Mj1unIyLXu55jcPmaVKe6WvWFXM8yJS3nMlnBDBxFwmtHkAmAuwqJ8irILXG170d6pMZ38STvAAqqMsWKZNI0rD00v9up0bzN
+
